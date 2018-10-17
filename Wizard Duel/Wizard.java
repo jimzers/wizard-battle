5a08f35bbdb4786393598571605b544dd6f9a26c
jimzers <adamleesaintfrancis@gmail.com>
2018-10-16 23:29:34 -0700

    debug push, fix order of damping effect

--- Wizard Duel/Wizard.java
+++ Wizard Duel/Wizard.java
@@ -51,22 +51,24 @@ public class Wizard
         if (someSpell.getSpell_Running()) {
             Random r = new Random();
             double attack = someSpell.getAttackRating();
+
+            if (someSpell.getDuration() != someSpell.getTurns()){
+                // calculating damping damage
+                double randDmg = (someSpell.getDamageDifference() / 10) * (Math.random() * 10) + someSpell.getLowDamage();
+                double damp_mod = Math.pow(someSpell.getDamping_factor(), (someSpell.getDuration() - someSpell.getTurns()));
+                double dmgcalc = randDmg * damp_mod;
+                health = health - dmgcalc;
+                JOptionPane.showMessageDialog(null, "Your attack " + someSpell.getName() + " had an aftereffect of " + dmgcalc + " damage.");
+            }
             // random chance calculation to see if attack hits
             if (attack >= ((r.nextInt(10) + 1) * 10)) {
                 // checks if duration has exceeded past first turn, if not then it runs regular damage code
-                if (someSpell.getDuration == someSpell.getTurns){
+                if (someSpell.getDuration != someSpell.getTurns){
                     // calculating 1st turn damage
                     double randDmg = (someSpell.getDamageDifference() / 10) * (Math.random() * 10) + someSpell.getLowDamage();
                     double dmgcalc = randDmg;
                     health = health - dmgcalc;
                     JOptionPane.showMessageDialog(null, "Your attack " + someSpell.getName() + " was a success!!!" + "It did " + dmgcalc + " damage.");
-                } else {
-                    // calculating damping damage
-                    double randDmg = (someSpell.getDamageDifference() / 10) * (Math.random() * 10) + someSpell.getLowDamage();
-                    double damp_mod = Math.pow(someSpell.getDamping_factor(), (someSpell.getDuration() - someSpell.getTurns()));
-                    double dmgcalc = randDmg * damp_mod;
-                    health = health - dmgcalc;
-                    JOptionPane.showMessageDialog(null, "Your attack " + someSpell.getName() + " had an aftereffect of " + dmgcalc + " damage.");
                 }
             } else {
                 // attack misses
