eca03d350f9c2d209a54c55b21bd572a50aca6e2
DeepSFHS <d4joshi@gmail.com>
2018-10-16 22:22:54 -0700

    Add files via upload

--- Wizard Duel/Starter.java
+++ Wizard Duel/Starter.java
@@ -13,7 +13,7 @@ public class Starter
      */
     public static void main(String[] args)
     {
-        // initialise instance variables
+        // initialize local variables needed for the purpose of code
         double days;
         double turn_no = 0;
         days = 10;
@@ -22,15 +22,17 @@ public class Starter
         ArrayList<Spell> spells = new ArrayList<Spell>();
         ArrayList<Integer> track1 = new ArrayList<Integer>();
         ArrayList<Integer> track2 = new ArrayList<Integer>();
+        
+        // creates five default spells 
         spells.add(new Spell("Water Gun", 100, 20, 25, 5, 1, 1));
         spells.add(new Spell("Thunderbolt", 100, 20, 25, 5, 1, 1));
         spells.add(new Spell("Solar Beam", 100, 20, 25, 5, 1, 1));
-        spells.add(new Spell("tiger mom", 25, 90, 100, 5, 1, 1));
-        spells.add(new Spell("poison fart", 50, 15, 20, 2, 3, 0.5));
+        spells.add(new Spell("Tiger Mom", 25, 90, 100, 5, 1, 1));
+        spells.add(new Spell("Poison Fart", 50, 15, 20, 2, 3, 0.5));
         
+        // this blocks asks the user to enter spells with their features beside the default spells and stops when the user says no 
         String answer = JOptionPane.showInputDialog("Do you want to add more spells than our current four");
         while (answer.equals("yes")) {
-           // Spell(String n, double a, double ld, double hd, double t, double du, double da)
            String n = JOptionPane.showInputDialog("What is the name of your spell?");
            Double a = Double.parseDouble(JOptionPane.showInputDialog("What is the attack rating?"));
            Double ld = Double.parseDouble(JOptionPane.showInputDialog("What is the low damage rating?"));
@@ -42,11 +44,13 @@ public class Starter
            answer = JOptionPane.showInputDialog("Do you still want to add more spells?");
         }
        
+        // prints out the spells and how many days it takes to learn each spell 
         for (int i = 0; i < spells.size(); i++) {
             System.out.println(Integer.toString(i+1) + ". " + (spells.get(i)).getName() + " - " + Double.toString((spells.get(i)).getTraining()
             ) + " days");
         }
         
+        // THE NEXT FEW BLOCKS OF CODE IS FOR STORING THE SPELLS LEARNED BY PLAYER ONE AND PLAYER TWO                                                 
         JOptionPane.showMessageDialog(null, "Look at Terminal Screen for spells and how many days it takes to learn each spell.");
         String firstwizard = JOptionPane.showInputDialog("Please enter the name for your first wizard.");
         playerone = new Wizard(firstwizard);
@@ -55,6 +59,7 @@ public class Starter
         boolean selectphase = true;
         boolean spellexists = false;
         int number;
+        
         while (selectphase) {
             // need to have while loop that checks to see if selected spell is under the allowed days
             while(true){
@@ -79,7 +84,7 @@ public class Starter
                 }
             }
             if (!spellexists){
-                JOptionPane.showMessageDialog(null, "lmaO THERE ain't no spells left");
+                JOptionPane.showMessageDialog(null, "There are no spells left to learn.");
                 break;
             }
             // iterate thru all the spells in spell list to see if they are under the today days left, if none match that condition it sets the phase to false
@@ -130,6 +135,8 @@ public class Starter
             Spell clonedspell = (Spell) spells.get(track2.get(i)).clone();
             playertwo.addSpell(clonedspell);
         }
+        
+        // creates instance of Game brain class to create a while loop that ends when game is over 
         GameBrain gB = new GameBrain(playerone, playertwo);
         while (gB.duel(playerone, playertwo) == true) {
             // to damage enemy: pick a spell, that starts the spell running thing
@@ -141,7 +148,7 @@ public class Starter
             System.out.println("Health: " + Double.toString(playerone.getHealth()));
             playerone.printSpells();
 
-
+            // asks and stores spell the first player chooses for his or her turn and then calls upon the second player to reduce the second player's health
             int spell1 = Integer.parseInt(JOptionPane.showInputDialog("Look at terminal for your available spells and damage. Select spell by number for your turn.")) - 1;
             playerone.getSpellbook().get(spell1).setRunningTrue();
             for(int k = 0; k < playerone.getSpellbook().size(); k++){
@@ -149,7 +156,8 @@ public class Starter
                     playertwo.damage((playerone.getSpellbook()).get(k));
                 }
             }
-
+            
+            // same as before but in terms of player two's point of view 
             System.out.print('\u000C');
             if (gB.duel(playerone, playertwo) == true) {
                 JOptionPane.showMessageDialog(null, secondwizard + " it is your turn.");
@@ -164,6 +172,8 @@ public class Starter
                 }
             }
         }
+        
+        // end game 
         System.out.println("Game Over!!!! " + gB.getWinner() + " won!!!"); 
     }
 
